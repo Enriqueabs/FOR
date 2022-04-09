9f795331da6e28dd80c37c7f5694ea317f999f7b
enrique becerra <enriquealejandrobs@hotmail.com>
2022-04-08 18:11:33 -0700

    Agregar archivos de proyecto.

---
+++ FOR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i, numA;
+            double calificacion, sumaCalif = 0, promedio;
+
+            Console.Write("Ingresa el numero de alumnos: ");
+            numA = Convert.ToByte(Console.ReadLine());
+
+            for (i = 1; i <= numAlumnos; i++)
+            {
+                Console.Write("Ingresa la calificacion: ");
+                calificacion = Convert.ToDouble(Console.ReadLine());
+
+                sumaCalif += calificacion;
+            }
+
+            promedio = sumaCalif / numA;
+            Console.WriteLine("El promedio es {0}", promedio);
+        }
+    }
+}
